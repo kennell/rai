a3529cfb5bc02080af74ab2761d1722d7ff1c5ef
Kevin Kennell <kevin.kennell@gotthardt.com>
2018-01-07 19:01:55 +0100

    node: add wallets property

--- rai/node.py
+++ rai/node.py
@@ -1,5 +1,5 @@
 from .rpc import make_rpc, get_connection
-
+from .wallet import Wallet
 
 class Node:
 
@@ -26,3 +26,15 @@ class Node:
     @property
     def connection(self):
         return get_connection()
+
+    @property
+    def wallets(self):
+        """
+            Returns all Wallets available on the node
+        """
+        rsp = make_rpc(
+            {
+                'action': 'wallet_list'
+            }
+        )
+        return [Wallet(id=wallet_id) for wallet_id in rsp['wallets']]
