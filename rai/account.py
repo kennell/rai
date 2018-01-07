3a519a63d3506bc0f9a38e36f93e1b8dd4b43e6c
Kevin Kennell <kevin.kennell@gotthardt.com>
2018-01-07 14:29:20 +0100

    account: implement balance, pending

--- rai/account.py
+++ rai/account.py
@@ -6,6 +6,32 @@ class Account:
     def __init__(self, address):
         self.address = address
 
+    def _get_balance(self):
+        rsp = make_rpc(
+            {
+                "action": "account_balance",
+                "account": self.address
+            }
+        )
+        return rsp['balance']
+
+    @property
+    def balance(self):
+        return self.get_balance()
+
+    def _get_pending(self):
+        rsp = make_rpc(
+            {
+                "action": "account_balance",
+                "account": self.address
+            }
+        )
+        return rsp['pending']
+
+    @property
+    def pending(self):
+        return self.get_pending()
+
     def __str__(self):
         return self.address
 
