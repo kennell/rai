b9a8a4fd7826bc37fe3d04f20d1a3d0b5b46f3fc
Kevin Kennell <kevin.kennell@gotthardt.com>
2018-01-07 14:29:30 +0100

    version: bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name = 'rai',
   packages = ['rai'],
-  version = '0.0.4',
+  version = '0.0.5',
   license = 'MIT',
   description = 'A high-level client for interacting with Raiblocks nodes',
   author = 'Kevin Kennell',
