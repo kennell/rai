2421e9be72cbdee5bde3e05e10e047a07de4c357
Kevin Kennell <kevin.kennell@gotthardt.com>
2018-01-20 18:21:17 +0100

    pep8

--- rai/node.py
+++ rai/node.py
@@ -4,19 +4,20 @@ from .rpc import make_rpc, get_connection
 class Node:
 
     def is_available(self):
+        """
+            Check if RPC node is available
+        """
         try:
-            make_rpc(
-                {
-                    'action': 'version'
-                }
-            )
+            self.version
             return True
-
         except Exception:
             return False
 
     @property
     def version(self):
+        """
+            Gets the version of the RPC node
+        """
         rsp = make_rpc(
             {
                 'action': 'version'
@@ -26,4 +27,7 @@ class Node:
 
     @property
     def connection(self):
+        """
+            Show the connection string
+        """
         return get_connection()
