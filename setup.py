09cab89106d3024a888bc58af28efa9350d81ad6
Kevin Kennell <kevin.kennell@gotthardt.com>
2018-01-20 18:15:40 +0100

    setup: add classifiers

--- setup.py
+++ setup.py
@@ -2,17 +2,25 @@ from setuptools import setup
 
 
 setup(
-  name='rai',
-  packages=['rai'],
-  version='0.0.10',
-  license='MIT',
-  description='A high-level, pythonic client for interacting with Raiblocks nodes',
-  author='Kevin Kennell',
-  author_email='kevin@kennell.de',
-  install_requires=[
-    'requests'
-  ],
-  url='https://github.com/kennell/rai',
-  keywords=['raiblocks', 'cryptocurrency'],
-  classifiers=[],
+	name='rai',
+	packages=['rai'],
+	version='0.0.10',
+	license='MIT',
+	description='A high-level, pythonic client for interacting with Raiblocks nodes',
+	author='Kevin Kennell',
+	author_email='kevin@kennell.de',
+	install_requires=[
+		'requests'
+	],
+	url='https://github.com/kennell/rai',
+	keywords=[
+		'raiblocks',
+		'cryptocurrency'
+	],
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6'
+	]
 )
